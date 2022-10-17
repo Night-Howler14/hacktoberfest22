d20b66d696efbfb644c9c0e2507e57435f87a569
yagami444 <115549230+yagami444@users.noreply.github.com>
2022-10-17 17:24:11 +0530

    Dequeue Linked List
    Please accept my solution.

---
+++ dequeue_linked_list.cpp
@@ -0,0 +1,17 @@
+//  dequeue_linked_list.cpp: Rishabh Dev
+//  Description: First implementation of dequeue using linked list.
+#include <iostream>
+using namespace std;
+
+struct DQNode
+{
+    int _data;
+    DQNode *_next, *_previous;
+    // DQNode(_data)
+};
+
+int main()
+{
+
+    return 0;
+}
