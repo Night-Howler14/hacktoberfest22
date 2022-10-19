8f179c39ec9879847d4404c4ce837108a46391ac
parimal <97539350+parimalongithub@users.noreply.github.com>
2022-10-19 15:49:11 +0530

    added factorial program in java

--- Library.java
+++ Library.java
@@ -1,132 +1,168 @@
-package lab3;
 import java.util.Scanner;
-class book{
-    private int bookid;
+
+class book {
+
+    private int bookId;
     private String bookTitle;
     private int yearOfPublication;
-    private String authorName;
+    private String authorname;
     private String publisherName;
     private int numberOfAvailableCopies;
-    public void setDetails(int id,String title, int year,String author, String publisher, int count){
-        bookid=id;
-        bookTitle=title;
-        yearOfPublication=year;
-        authorName=author;
-        publisherName=publisher;
-        numberOfAvailableCopies=count;
-    }
-    public void getDetails(int id){
-        bookid=id;
-    System.out.println("Book Name: "+bookTitle + ", "+"Publication Year: "+yearOfPublication+ ",  "+"Author: "+authorName+",  "+"Publisher: "+publisherName+",  "+"Available Copies: "+numberOfAvailableCopies);
-    }
-    public int check(){
-        return bookid; 
-    }
-    public void issue(int id){
-        bookid=id;
-        numberOfAvailableCopies--;
-    }
-    public void Return(int id){
-        bookid=id;
-        numberOfAvailableCopies++;
-    }
-}
-public class Library{
-    public static void main(String args[]){
-        book arr[];
-        int y=5;
-        arr=new book[10];
-        arr[0]=new book();
-        arr[1]=new book();
-        arr[2]=new book();
-        arr[3]=new book();
-        arr[4]=new book();
-        
-        arr[0].setDetails(01,"Adventures of Tom Sawyer",2001," Mark Twain","Time magazine",5);
-        arr[1].setDetails(02,"Agni Veena",2003,"xyz","time of india",6);
-        arr[2].setDetails(03,"gfg",2004,"abc","hindustan",2);
-        arr[3].setDetails(04,"harry potter",2021,"qwers","Flowthorn",9);
-        arr[4].setDetails(05,"Twilight",1999,"Stephanie Meyer","Beauxbatons",4);
+    private int totalCopies;
 
-        Scanner input=new Scanner(System.in);
-        int option=0;
-        while(option!=5){
-            System.out.println("Enter 1 to set details");
-            System.out.println("Enter 2 to get details");
-            System.out.println("Enter 3 to issue book");
-            System.out.println("Enter 4 to return book");
-            System.out.println("Enter 5 to exit");
-            option=input.nextInt();
-            switch(option){
-                case 1: 
-                System.out.println("Enter the BookId: ");
-                int id=input.nextInt();
-                System.out.println("Enter the   Book Name: ");
-                String name=input.nextLine();
-                name=input.nextLine();
-                System.out.println("Enter the year: ");
-                int year=input.nextInt();
-                System.out.println("Enter the author: ");
-                String author=input.nextLine();
-                author=input.nextLine();
-                System.out.println("Enter the publisher: ");
-                String publisher=input.nextLine();
-                System.out.println("Enter the copy count: ");
-                int count=input.nextInt();
-                arr[y]=new book();
-                arr[y].setDetails(id,name,year,author,publisher,count);
-                y++;
-                break;
-    
-                case 2: 
-                System.out.println("Enter the id: ");
-                id=input.nextInt();
-                for(int i=0;i<10;i++){
-                    if(arr[i].check()==id){
-                        arr[i].getDetails(id);
+    book(int copies) {
+        totalCopies = copies;
+    }
+
+    public int checkid(int id) {
+        if (id == bookId) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public void setDetails(int id, String title, int year, String author, String publisher, int count) {
+
+        bookId = id;
+        bookTitle = title;
+        yearOfPublication = year;
+        authorname = author;
+        publisherName = publisher;
+        numberOfAvailableCopies = count;
+
+    }
+
+    public void getDetails(int id) {
+        if (id == bookId) {
+            System.out.println("**********************************************");
+            System.out.println("\tId : " + bookId);
+            System.out.println("\tTitle : " + bookTitle);
+            System.out.println("\tYear : " + yearOfPublication);
+            System.out.println("\tAuthor : " + authorname);
+            System.out.println("\tPublisher : " + publisherName);
+            System.out.println("\tAvailable copies : " + numberOfAvailableCopies);
+            System.out.println("\tTotalCopies : " + totalCopies);
+            System.out.println("***********************************************");
+        }
+    }
+
+    public void issue(int id) {
+        if (id == bookId) {
+            if (numberOfAvailableCopies > 0) {
+
+                numberOfAvailableCopies--;
+                System.out.println("Issued -- Available copies : " + numberOfAvailableCopies);
+            } else {
+                System.out.println("Can't Issue as the copy is Not available at the moment!");
+            }
+        }
+    }
+
+    public void returning(int id) {
+        if (id == bookId) {
+            if (numberOfAvailableCopies != totalCopies) {
+
+                numberOfAvailableCopies++;
+                System.out.println("Returned -- Available copies : " + numberOfAvailableCopies);
+            } else {
+                System.out.println("Can't Return as ( Available copies = Total copies )");
+            }
+        }
+    }
+
+}
+
+public class library {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        book[] arr;
+        arr = new book[5];
+        int i = 0;
+        while (true) {
+            System.out.println("\t ---------------- ");
+            System.out.println("\t|1 - setDetails()|");
+            System.out.println("\t|2 - getDetails()|");
+            System.out.println("\t|3 - issue()     |");
+            System.out.println("\t|4 - Return()    |");
+            System.out.println("\t|5 - Exit()      |");
+            System.out.println("\t ---------------- ");
+
+            System.out.println("******************");
+            System.out.println("Enter your choice");
+            System.out.println("******************");
+            int choice = input.nextInt();
+
+            if (choice == 1) {
+
+                System.out.println("Enter details for book " + (i + 1));
+                System.out.println("Enter Total Copies");
+                int x = input.nextInt();
+                arr[i] = new book(x);
+
+                System.out.println("Enter Id");
+                int id = input.nextInt();
+                input.nextLine();
+
+                System.out.println("Enter Title");
+                String title = input.nextLine();
+
+                System.out.println("Enter Year");
+                int year = input.nextInt();
+
+                System.out.println("Enter Author");
+                input.nextLine();
+                String author = input.nextLine();
+
+                System.out.println("Enter Publisher");
+                String publisher = input.nextLine();
+
+                System.out.println("Enter Count");
+                int count = input.nextInt();
+                int flag = 0;
+
+                for (int j = 0; j < i; j++) {
+                    flag = arr[j].checkid(id);
+                    if (flag == 1) {
                         break;
                     }
-                    else{
-                        continue;
-                    }
                 }
-                break;
-    
-                case 3: 
-                System.out.println("Enter the id of the issued book: ");
-                id=input.nextInt();
-                for(int i=0;i<10;i++){
-                    if(arr[i].check()==id){
-                        arr[i].issue(id);
-                        break;
-                    }
-                    else{
-                       continue;
-                    }
+                if (flag == 0) {
+                    arr[i].setDetails(id, title, year, author, publisher, count);
+                    i++;
+                } else {
+                    System.out.println("I'd already exist!");
                 }
-                break;
-    
-                case 4: 
-                System.out.println("Enter the id of the returned book: ");
-                id=input.nextInt();
-                for(int i=0;i<10;i++){
-                    if(arr[i].check()==id){
-                       arr[i].Return(id);
-                        break;
-                    }
-                    else{
-                        continue;
-                    }
+
+            } else if (choice == 2) {
+                System.out.println("Enter id To Get the details of any book");
+                int id1 = input.nextInt();
+                for (int j = 0; j < i; j++) {
+                    arr[j].getDetails(id1);
                 }
+
+            } else if (choice == 3) {
+                System.out.println("Enter id to issue book");
+                int id1 = input.nextInt();
+                for (int j = 0; j < i; j++) {
+                    arr[j].issue(id1);
+                }
+
+            } else if (choice == 4) {
+                System.out.println("Enter id to return book");
+                int id1 = input.nextInt();
+                for (int j = 0; j < i; j++) {
+                    arr[j].returning(id1);
+                }
+            }
+
+            else if (choice == 5) {
                 break;
-    
-                default:
-                	System.out.println("The entered option is invalid.... Please Re-enter the option :");
-                
-            } 
-        }  
+            }
+        }
+
         input.close();
     }
-    
-  
 }
